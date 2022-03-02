2af48e46bff1b3fd9f125b350856f40456b2960f
Alvaro Moya Herraiz <alvaro.moya.external@atos.net>
2022-03-02 18:46:30 +0100

    Add project files.

---
+++ PostTutorialNET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PostTutorialNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
